8a89e64a59133e1c0cbf26c58430d85959de0704
Hongdonggeon <ghd700@naver.com>
2021-12-16 04:43:38 +0900

    feat(station) : 이름 길이 검사하기

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,9 +1,11 @@
 package subway.domain;
 
 public class Station {
+    private static final String STATION_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 이름은 최소 두글자 이상이여야 한다.";
     private String name;
 
     public Station(String name) {
+        validateNameLength(name);
         this.name = name;
     }
 
@@ -12,4 +14,9 @@ public class Station {
     }
 
     // 추가 기능 구현
+    public static void validateNameLength(String name) {
+        if (name.length() < 2) {
+            throw new IllegalArgumentException(STATION_NAME_LENGTH_ERROR_MESSAGE);
+        }
+    }
 }
