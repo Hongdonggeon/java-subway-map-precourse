b2fbc4f43c685386c58d844f099074d7baad59d0
Hongdonggeon <ghd700@naver.com>
2021-12-16 06:01:11 +0900

    feat(inputview) : 지하철 노선 입력 기능

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -6,18 +6,17 @@ public class InputView {
 	public static final String USER_CHOISE_FUNCTION_ALARM_MESSAGE = "## 원하는 기능을 선택하세요";
 	public static final String STATION_SAVE_ALARM_MESSAGE = "## 등록할 역 이름을 입력하세요";
 	public static final String STATION_DELETE_ALARM_MESSAGE = "## 삭제할 역 이름을 입력하세요";
+	public static final String LINE_SAVE_ALARM_MESSAGE = "## 등록할 노선 이름을 입력하세요";
+	public static final String LINE_UP_TERMINAL_ALARM_MESSAGE = "## 등록할 노선의 상행 종점역 이름을 입력하세요";
+	public static final String LINE_DOWN_TERMINAL_ALARM_MESSAGE = "## 등록할 노선의 하행 종점역 이름을 입력하세요";
+	public static final String LINE_DELETE_ALARM_MESSAGE = "## 삭제할 노선 이름을 입력하세요";
 
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static String inputString() {
 		return scanner.next();
 	}
-	public static String selectMainFunction() {
-		System.out.println(USER_CHOISE_FUNCTION_ALARM_MESSAGE);
-		return inputString();
-	}
-
-	public static String selectStationFunction() {
+	public static String selectWantedFunction() {
 		System.out.println(USER_CHOISE_FUNCTION_ALARM_MESSAGE);
 		return inputString();
 	}
@@ -31,6 +30,30 @@ public class InputView {
 	public static String inputStationToDelete() {
 		System.out.println();
 		System.out.println(STATION_DELETE_ALARM_MESSAGE);
+		return inputString();
+	}
+
+	public static String inputLineToSave() {
+		System.out.println();
+		System.out.println(LINE_SAVE_ALARM_MESSAGE);
+		return inputString();
+	}
+
+	public static String inputLineUpTerminalToSave() {
+		System.out.println();
+		System.out.println(LINE_UP_TERMINAL_ALARM_MESSAGE);
+		return inputString();
+	}
+
+	public static String inputLineDownTerminalToSave() {
+		System.out.println();
+		System.out.println(LINE_DOWN_TERMINAL_ALARM_MESSAGE);
+		return inputString();
+	}
+
+	public static String inputLineToDelete() {
+		System.out.println();
+		System.out.println(LINE_DELETE_ALARM_MESSAGE);
 		return inputString();
 	}
 
